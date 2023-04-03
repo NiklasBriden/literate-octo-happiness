13f5477a1033d1ef131e824667edb4af2457160b
NiklasBriden <126027728+NiklasBriden@users.noreply.github.com>
2023-04-03 12:06:19 +1200

    Done function

---
+++ src/JSPractice.js
@@ -0,0 +1,12 @@
+// Create a function that takes an array with only numbers and returns the first element.
+let x = 6
+console.log("Hello World!")
+function myArrayfunc(arr) {
+    return arr[1][3]
+}
+if(x == 5) {
+    console.log("True")
+}
+
+console.log(myArrayfunc([["Niklas"],[1, 2, 3, 4, 5, 6, 7]]))
+
